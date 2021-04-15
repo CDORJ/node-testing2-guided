c5ea2c5202ddf8829c7a53115b613fbc38999355
ChadDiaz <chadcdiaz@gmail.com>
2021-04-15 17:39:05 -0400

    working on deploy

--- knexfile.js
+++ knexfile.js
@@ -23,15 +23,17 @@ module.exports = {
     client: "pg",
     connection: {
       connectionString: pgConnection,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-      pool: {
-        min: 2,
-        max: 10,
-      },
-      migrations: { directory: "./data/migrations" },
-      seeds: { directory: "./data/seeds" },
+      ssl: { rejectUnauthorized: false },
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
     },
   },
 };
